3e266d69f51abc0ce947256b8404543bcbb6d561
Muhammed Rifan <rifanmuhammed173@gmail.com>
2026-04-05 11:47:24 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ app.get('/', (req, res) => {
     res.status(200).json({
         success: true,
         message: 'Welcome to MentorBro API',
-        version: '1.0.18',
+        version: '1.0.19',
         documentation: '/api/v1/docs',
     })
 });
